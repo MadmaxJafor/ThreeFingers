fbcc6bf52ca15abab4f39c6ec697d8592f2aebd7
Mohamedecorot <guerroui.m@gmail.com>
2020-03-24 14:48:18 +0100

    ajout du css pour le formulaire de contact

--- espace_de_travail2.php
+++ espace_de_travail2.php
@@ -5,13 +5,13 @@
 Là où Mohamed travail
 
 <section id="formulaire-contact">
-    <h2>Pour nous contacter, n'hésitez pas à remplir le formulaire suivant</h2>
+    <h2>Pour nous contacter, n'hésitez pas à remplir le formulaire suivant: </h2>
     <form action="#formulaire-contact" method="POST">
     <input type="text" name="nom" required placeholder="entrez votre nom">
     <input type="text" name="prenom" required placeholder="entrez votre prenom">
     <input type="text" name="raison" placeholder="entrez la raison de votre message">
     <textarea name="message" cols="60" rows="6" required placeholder="entrez votre message"></textarea>
-    <button type="submit">Envoyez votre message</button>
+    <button id="bouton" type="submit">Envoyez votre message</button>
 
     </form>
 </section>
