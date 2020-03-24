885cd19f968d07c66923da57bd17f532e6d5add3
pgawebdev <pg.archen.webdev@gmail.com>
2020-03-24 16:27:45 +0100

    ajout listing dates + CSS + réupload image

--- espace_de_travail3.php
+++ espace_de_travail3.php
@@ -1,10 +1,31 @@
 <h2>Espace de travail 3</h2>
+Là où PG travail.
 
-Nos dates de concert à venir :
-<div>
-<img src="assets/img/calendrier.png" alt="calendrier" >
+
+<div id="calendrier">
+    <div id="mainDates">
+        <h2>Nos dates de concert à venir :</h2>
+        <img src="assets/img/calendrier.png" alt="calendrier" >
+    </div>
+    <div id="sideDates">
+        <h3>Dates passées</h3>
+        <ul>
+            <li>4 Février -Le Moulin, Marseille</li>
+            <li>19 Février - Le Korrigan, Lyunes</li>
+            <li>20 Mars -L'Usine, Istres</li>
+        </ul>
+        <h3>Aujourd'hui</h3>
+        <ul>
+            <li>Désolé, aucun concert prévus ce jour.</li>
+        </ul>
+        <h3>Dates à venir</h3>
+        <ul>
+            <li>27 Mars - Le Coco Velten, Marseille</li>
+            <li>19 Janvier - Brasserie Communale, Marseille</li>
+        </ul>
+        
+    </div>
 </div>
-
 </main>
     <footer>
         <p>Tous droits réservés</p>
