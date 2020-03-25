13565902cccca39813b131d5fd75b1ceede77173
MadmaxJafor <piashadome@gmail.com>
2020-03-25 13:46:53 +0100

    button

--- espace_de_travail1.php
+++ espace_de_travail1.php
@@ -1,3 +1,19 @@
-<h2>Espace de travail 1</h2>
-Là où Jafor travail
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="button">
+        <form id="search-form" name="search" action="/products" method="get">
+            <input id="search-input" name="search" type="text">
+            <input src="path/to/image" name="submit" type="image">
+        </form>
+    </div>
+</body>
+
+</html>
