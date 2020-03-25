b10a7183710e5378892f12c8642188f1925c2369
pgawebdev <pg.archen.webdev@gmail.com>
2020-03-25 14:21:48 +0100

    début fonction calendrier

--- php/functions3.php
+++ php/functions3.php
@@ -1,2 +1,24 @@
 <?php
+
+function constructCalendrier($nbJour)
+{
+    
+    $i=0;
+    
+    $raw = $nbJour%10
+    
+    for($iRaw= 0; $iRaw < $raw; $iRaw++)
+    {
+        echo 
+        <tr>
+            if($i<$nbJour)
+        
+        </tr>;
+    }
+
+}
+
+ $calendrier = constructCalendrier(30);
+ echo $calendrier;
+
 ?>
