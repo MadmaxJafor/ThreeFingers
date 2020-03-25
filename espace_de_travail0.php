aa40d668578eb44069c223395d64938e2203a3f0
Mohamedecorot <guerroui.m@gmail.com>
2020-03-25 14:22:47 +0100

    présentation fictive et index de navigation

--- espace_de_travail0.php
+++ espace_de_travail0.php
@@ -21,11 +21,14 @@
         </div>
         <nav>
             <ul>
-                <li><a href="#">Présentation</a></li>
-                <li><a href="#">Musique</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Calendrier</a></li>
+                <li><a href="#s0">Présentation</a></li>
+                <li><a href="#s1">Musique</a></li>
+                <li><a href="#s2">Contact</a></li>
+                <li><a href="#s3">Calendrier</a></li>
             </ul>
         </nav>
     </header>
     <main>
+        <section id="s0">
+        <h2> Présentation </h2>
+        <p>Exemple :Le groupe En Passant Par la Montagne est crée en 2009 au sein de la Cie « Bal’us’Trad » née en 2001.</br> Les quatre musiciens amoureux de sons acoustiques se rassemblent autour de musiques suédoises et travaillent leurs propres compositions.</br> Le groupe propose chaque année un répertoire renouvelé, plein de fraicheur et d’humour, et se plaît à jouer avec le public pour un moment musical unique, poétique, participatif et fantaisiste. </br>Leur troisième album « l’Été » sort en avril 2016.</p>
