4e1373046d8fedb89535fb88258759b4a4c839a6
pgawebdev <pg.archen.webdev@gmail.com>
2020-03-30 15:59:43 +0200

    modi text pres

--- espace_de_travail0.php
+++ espace_de_travail0.php
@@ -28,13 +28,12 @@
             </ul>
         </nav>
     </header>
-    
-        <section id="s0">
-        
-            <img src="assets/img/background.jpg" alt="image de groupe">
-            <div class="content">
-                <h2> Présentation </h2>
-                <p>Exemple :Le groupe En Passant Par la Montagne est crée en 2009 au sein de la Cie « Bal’us’Trad » née en 2001.</br> Les quatre musiciens amoureux de sons acoustiques se rassemblent autour de musiques suédoises et travaillent leurs propres compositions.</br> Le groupe propose chaque année un répertoire renouvelé, plein de fraicheur et d’humour, et se plaît à jouer avec le public pour un moment musical unique, poétique, participatif et fantaisiste. </br>Leur troisième album « l’Été » sort en avril 2016.</p>
-            </div>    
-        </section>
         <main>
+            <section id="s0">
+                <img src="assets/img/background.jpg" alt="image de groupe">
+                <div class="content">
+                    <h2> Présentation </h2>
+                    <p>Exemple :Le groupe En Passant Par la Montagne est crée en 2009 au sein de la Cie « Bal’us’Trad » née en 2001.</br> Les quatre musiciens amoureux de sons acoustiques se rassemblent autour de musiques suédoises et travaillent leurs propres compositions.</br> Le groupe propose chaque année un répertoire renouvelé, plein de fraicheur et d’humour, et se plaît à jouer avec le public pour un moment musical unique, poétique, participatif et fantaisiste. </br>Leur troisième album « l’Été » sort en avril 2016.</p>
+                </div>    
+            </section>
+        
