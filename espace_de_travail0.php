f1acf44f4cd743a51f7a6f0baed13ffcc73ba5c9
pgawebdev <pg.archen.webdev@gmail.com>
2020-03-30 16:44:09 +0200

    css

--- espace_de_travail0.php
+++ espace_de_travail0.php
@@ -27,11 +27,11 @@
                 <li><a href="#s3">Calendrier</a></li>
             </ul>
         </nav>
-    </header>
-        <main>
+    </header><img src="assets/img/background.jpg" alt="image de groupe">
+        <main class="content">
             <section id="s0">
-                <img src="assets/img/background.jpg" alt="image de groupe">
-                <div class="content">
+                
+                <div >
                     <h2> Présentation </h2>
                     <p>Exemple :Le groupe En Passant Par la Montagne est crée en 2009 au sein de la Cie « Bal’us’Trad » née en 2001.</br> Les quatre musiciens amoureux de sons acoustiques se rassemblent autour de musiques suédoises et travaillent leurs propres compositions.</br> Le groupe propose chaque année un répertoire renouvelé, plein de fraicheur et d’humour, et se plaît à jouer avec le public pour un moment musical unique, poétique, participatif et fantaisiste. </br>Leur troisième album « l’Été » sort en avril 2016.</p>
                 </div>    
