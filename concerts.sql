7cbbe5e567edab7a2b284e3ea66ebb0f31b2d958
pgawebdev <pg.archen.webdev@gmail.com>
2020-04-06 22:24:10 +0200

    export table concerts

---
+++ concerts.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  lun. 06 avr. 2020 à 20:23
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `threefingers`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `concerts`
+--
+
+DROP TABLE IF EXISTS `concerts`;
+CREATE TABLE IF NOT EXISTS `concerts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lieu` varchar(150) NOT NULL,
+  `date` date NOT NULL,
+  `ville` varchar(250) NOT NULL,
+  `adresse` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
