6f53600b48a89ed6c18218647d764e7b9a3bfec8
pgawebdev <pg.archen.webdev@gmail.com>
2020-04-06 22:00:45 +0200

    organisation / 1ere centralisation de code

--- php/functions.php
+++ php/functions.php
@@ -1,2 +1,51 @@
 <?php
+
+function filtrer($name="id")
+{
+    $resultat = $_REQUEST[$name] ?? "";
+    return $resultat;
+}
+
+
+// CREATE 
+if ($identifiantFormulaire == "create")
+{
+    
+    $tabAssoColonneValeur = [
+        "nom"            => filtrer("nom"),
+        "prenom"         => filtrer("prenom"),
+        "email"          => filtrer("email"),
+        "raison"         => filtrer("raison"),
+        "message"        => filtrer("message"),
+    ];
+    
+    extract($tabAssoColonneValeur);
+
+    if ($nom != "" 
+        && $prenom != ""
+        && $email != ""
+        && $raison != ""
+        && $message != "")
+    {
+        $requeteSQL   =
+<<<CODESQL
+
+INSERT INTO OnePage
+( nom, prenom, email, raison, message)
+VALUES
+( :nom, :prenom, :email, :raison, :message) 
+
+CODESQL;
+
+echo "$nom, $prenom, $email, $raison, $message" ;
+        require_once "connectionDb.php";
+
+        echo "Votre message a bien été envoyé, nous vous recontacterons bientôt. Merci. ";
+    }
+    else
+    {
+        echo "VEUILLEZ REMPLIR TOUS LES CHAMPS OBLIGATOIRES S'IL VOUS PLAIT !!!";
+    }
+
+}
 ?>
