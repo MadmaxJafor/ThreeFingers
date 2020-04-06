928131dbc6bdda34f4272fb40932f031ee7513de
pgawebdev <pg.archen.webdev@gmail.com>
2020-04-06 22:04:15 +0200

    suppression fichiers obsolètes

--- fonctionrequeteSQL.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$pdo = new PDO("mysql:host=localhost;dbname=ThreeFingers;charset=utf8;", "root", "");
-
-$pdoStatement = $pdo->prepare($requeteSQL);
-
-$pdoStatement->execute($tabAssoColonneValeur);
-
-
--- js/app3.js
+++
@@ -1,1 +0,0 @@
-console.log ("chargement en cours ....");
