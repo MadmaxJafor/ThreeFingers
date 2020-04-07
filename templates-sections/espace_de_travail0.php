b04f0eb81ccca22da4674f8a76325a2a735b793c
pgawebdev <pg.archen.webdev@gmail.com>
2020-04-07 12:12:53 +0200

    modif présentation

--- templates-sections/espace_de_travail0.php
+++ templates-sections/espace_de_travail0.php
@@ -3,7 +3,6 @@
                 
                 <div >
                     <h2> Présentation </h2>
-                    <p>Exemple :Le groupe En Passant Par la Montagne est crée en 2009 au sein de la Cie « Bal’us’Trad » née en 2001.</br> Les quatre musiciens amoureux de sons acoustiques se rassemblent autour de musiques suédoises et travaillent leurs propres compositions.</br> Le groupe propose chaque année un répertoire renouvelé, plein de fraicheur et d’humour, et se plaît à jouer avec le public pour un moment musical unique, poétique, participatif et fantaisiste. </br>Leur troisième album « l’Été » sort en avril 2016.</p>
+                    <p>Deuxième collaboration musicale entre Julien Poule et Vincent Nasri après l'aventure Coda.</br> Ils reviennent avec beaucoup d'envie et des idées qui font bouger</p>
                 </div>    
             </section>
-        
